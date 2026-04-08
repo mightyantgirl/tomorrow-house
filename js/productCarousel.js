c3e19e633a518efb456361dbe61c46124e2b761b
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-04-08 23:53:06 +0900

    carousel js 생성

---
+++ js/productCarousel.js
@@ -0,0 +1,12 @@
+const slider = tns({
+  container: '.product-carousel .silder-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsthumbnail: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
